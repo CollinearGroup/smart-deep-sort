5d371ea25c54bcba9768320c00f7ae8efb9a9d43
Teresa Trail <ttrail@collineargroup.com>
2015-12-09 15:22:25 -0800

    modified test criteria to insure field order by using stringify

--- spec/arraySortSpec.js
+++ spec/arraySortSpec.js
@@ -3,12 +3,12 @@ var sortBy = require('../index') ,
 
 describe('First level keys sort', function() {
   var simpleObj = { 'b': 'bb', 'a': 'aa', 'c': undefined, 'd': 'dd' }
-
+  
   it('sorts when all fields are defined', function() {
     var result = sortBy(simpleObj)
     expect(Object.keys(result).length).toBe(4)
-    expect(Object.keys(result)).toEqual(['a', 'b', 'c', 'd'])
-	  expect(result['d']).toBe('dd')
+    expect(JSON.stringify(result)).toEqual({'a': 'aa', 'b': 'bb', 'c': undefined, 'd': 'dd'})
+	  expect(JSON.stringify(result['d'])).toEqual('dd')
   })
 
   it('maintains undefined fields', function() {
@@ -20,31 +20,31 @@ describe('First level keys sort', function() {
 describe('Sort by key then inner arrays\' first field values', function() {
   it('sort handles strings and undefined fields', function() {
     var result = sortBy(data.musicStoresWithStringArray)
-	  expect(result).toEqual(data.sortedMusicStoresWithStringArray)
+	  expect(JSON.stringify(result)).toEqual(JSON.stringify(data.sortedMusicStoresWithStringArray))
   })
 
   it('sort handles number values', function() {
     var result = sortBy(data.teamsWithNumberArray)
-	  expect(result).toEqual(data.sortedTeamsWithNumberArray)
+	  expect(JSON.stringify(result)).toEqual(JSON.stringify(data.sortedTeamsWithNumberArray))
   })
 
   it('sort is stable', function() {
     var result = sortBy(data.stableCheck)
-	  expect(result).toEqual(data.sortedStableCheck)
+	  expect(JSON.stringify(result)).toEqual(JSON.stringify(data.sortedStableCheck))
   })
 
   it('sort handles nested objects', function() {
     var result = sortBy(data.musicStoresWithObjectArray)
-	  expect(result).toEqual(data.sortedMusicStoresWithObjectArray)
+	  expect(JSON.stringify(result)).toEqual(JSON.stringify(data.sortedMusicStoresWithObjectArray))
   })
 
   it('sort handles date strings in ISO 8601 format', function() {
     var result = sortBy(data.birthdays)
-	  expect(result).toEqual(data.sortedBirthdays)
+	  expect(JSON.stringify(result)).toEqual(JSON.stringify(data.sortedBirthdays))
   })
 
   it('sort handles CJK characters', function() {
     var result = sortBy(data.cjk)
-	  expect(result).toEqual(data.sortedcjk)
+	  expect(JSON.stringify(result)).toEqual(JSON.stringify(data.sortedcjk))
   })
 })
