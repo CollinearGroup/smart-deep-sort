0769965dd2cfe81daa1db2d7f45e4ff27ed728d6
Dan Villa <dvilla@collineargroup.com>
2019-04-06 19:25:24 -0700

    chore: update dependencies and tests

--- spec/arraySortSpec.spec.js
+++ spec/arraySortSpec.spec.js
@@ -1,5 +1,5 @@
 var sortBy = require('../lib/index')
-var util = require('util')
+let { expect } = require('chai')
 
 describe('First level keys sort', function() {
   var simpleObj = {
@@ -11,19 +11,19 @@ describe('First level keys sort', function() {
 
   it('sorts when all fields are defined', function() {
     var result = sortBy(simpleObj)
-    expect(Object.keys(result).length).toBe(4)
-    expect(JSON.stringify(result)).toBe(JSON.stringify({
+    expect(Object.keys(result).length).to.equal(4)
+    expect(JSON.stringify(result)).to.equal(JSON.stringify({
       'a': 'aa',
       'b': 'bb',
       'c': undefined,
       'd': 'dd'
     }))
-    expect(JSON.stringify(result['d'])).toBe(JSON.stringify('dd'))
+    expect(JSON.stringify(result['d'])).to.equal(JSON.stringify('dd'))
   })
 
   it('maintains undefined fields', function() {
     var result = sortBy(simpleObj)
-    expect(result['c']).not.toBeDefined()
+    expect(result['c']).to.not.exist
   })
 })
 
@@ -37,32 +37,32 @@ describe('Sort by key then inner arrays\' first field values', function() {
   })
   it('sort handles strings and undefined fields', function() {
     var result = sortBy(data.musicStoresWithStringArray)
-    expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedMusicStoresWithStringArray))
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedMusicStoresWithStringArray))
   })
 
   it('sort handles number values', function() {
     var result = sortBy(data.teamsWithNumberArray)
-    expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedTeamsWithNumberArray))
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedTeamsWithNumberArray))
   })
 
   it('sort is stable', function() {
     var result = sortBy(data.stableCheck)
-    expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedStableCheck))
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedStableCheck))
   })
 
   it('sort handles nested objects', function() {
     var result = sortBy(data.musicStoresWithObjectArray)
-    expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedMusicStoresWithObjectArray))
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedMusicStoresWithObjectArray))
   })
 
   it('sort handles date strings in ISO 8601 format', function() {
     var result = sortBy(data.birthdays)
-    expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedBirthdays))
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedBirthdays))
   })
 
   it('sort handles CJK characters', function() {
     var result = sortBy(data.cjk)
-    expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedcjk))
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedcjk))
   })
 })
 
@@ -78,11 +78,11 @@ describe('SmartDeepSort async', function() {
     var opts = {}
     var after = false
     sortBy(data.musicStoresWithStringArray, opts, function(result) {
-      expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedMusicStoresWithStringArray))
+      expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedMusicStoresWithStringArray))
       after = true
       done()
     })
-    expect(after).toBe(false)
+    expect(after).to.equal(false)
   });
 
 });
@@ -101,13 +101,13 @@ describe('SmartDeepSort variant objects', function() {
 
   it('should sort example object', function() {
     var result = sortBy(data.mixedTypesCheck)
-    expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedMixedTypesCheck))
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedMixedTypesCheck))
   });
 
   it('should sort by type when mixed', function() {
     var result1 = sortBy(data.mixedTypesWithStringFirst)
     var result2 = sortBy(data.mixedTypesWithArrayFirst)
-    expect(JSON.stringify(result1)).toBe(JSON.stringify(result2))
+    expect(JSON.stringify(result1)).to.equal(JSON.stringify(result2))
   });
 
 });
@@ -125,6 +125,6 @@ describe('SmartDeepSort arrays', function() {
 
   it('should sort example object', function() {
     var result = sortBy(data.arrayOfThings)
-    expect(JSON.stringify(result)).toBe(JSON.stringify(data.sortedArrayOfThings))
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(data.sortedArrayOfThings))
   });
 });
